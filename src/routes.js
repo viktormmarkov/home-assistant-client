e815439826d4e92f5038e78dd382c8b204e311ba
Victor Markov <vikata3@gmail.com>
2019-10-08 17:49:30 +0300

    Add users page

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import Dashboard from './components/dashboard/Dashboard'
 import Users from './components/users/Users'
+import User from './components/users/User'
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
-  { path: '/users', name: 'Users', component: Users }
+  { path: '/users', exact: true, name: 'Users', component: Users },
+  { path: '/users/:id', exact: true, name: 'User Details', component: User },
 ];
 
 export default routes;
--- src/services/userService.js
+++ src/services/userService.js
@@ -4,6 +4,9 @@ class UserService {
     query() {
         return apiBase.get('/users');
     }
+    getItem(id) {
+        return apiBase.get(`/users/${id}`);
+    }
 }
 
 export default new UserService();
