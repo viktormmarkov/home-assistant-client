45a1595e1a5d2f6167d6b1a1c17198822f387b4a
Victor Markov <vikata3@gmail.com>
2019-11-12 17:34:31 +0200

    Small refactoring for promotions

--- src/components/promotions/Promotions.js
+++ src/components/promotions/Promotions.js
@@ -1,71 +1,12 @@
 import React, { Component } from 'react';
-import { Table, Button } from 'reactstrap';
-import { Link } from 'react-router-dom';
-
+import EntityListBaseComponent from '../common/EntityListBaseComponent';
 import promotionService from '../../services/promotionService';
 
-function PromotionRow(props) {
-  const promotion = props.promotion
-
-  return (
-    <tr key={promotion._id}>
-      <td><Link to={`/promotions/${promotion._id}`}>{promotion.name}</Link></td>
-      <td>{promotion.createdAt}</td>
-      <td>{promotion._id}</td>
-    </tr>
-  )
-}
 class Promotions extends Component {
-  constructor() {
-    super();
-    this.state = {
-      promotions: [],
-      loading: false
-    };
-  }
-  componentDidMount() {
-    this.getItems();
-  }
-  getItems() {
-    this.setState({loading: true});
-    promotionService.query()
-      .then(res => {
-        this.setState({promotions: res.data, loading: false});
-      })
-      .catch(err => {
-        this.setState({loading: false});
-      })
-  }
-
-  addItem = () => {
-    this.props.history.push("/promotions/new");
-  }
-
   render() {
-    const {promotions} = this.state
     return (
-      <div className="animated fadeIn">
-          <div className="section-header">
-            <h3 class="inline">Promotions</h3>
-            <Button onClick={this.addItem} className="fright btn-sm">Add</Button>
-          </div>
-
-          <Table responsive hover>
-            <thead className="thead-light">
-              <tr>
-                <th>Name</th>
-                <th>Date Created</th>
-                <th>Id</th>
-              </tr>
-            </thead>
-            <tbody>
-                { promotions.map((promotion, index) => 
-                    <PromotionRow key={index} promotion={promotion} />) 
-                }
-              </tbody>
-          </Table>
-      </div>
-    );
+      <EntityListBaseComponent service={promotionService} entityName={'promotions'} {...this.props}/>
+    )
   }
 }
 
