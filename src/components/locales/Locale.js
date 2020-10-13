0d75170380aa3a1c2d4a1e5acd45250e865e3b92
Victor Markov <victor.markov@urbanise.com>
2020-10-13 22:23:50 +0300

    Improve locale UI editing

--- src/components/locales/Locale.js
+++ src/components/locales/Locale.js
@@ -2,27 +2,44 @@
 import React from 'react';
 import * as _ from 'lodash';
 import { connect } from 'react-redux';
-import { FormGroup, Button, Input, Label } from 'reactstrap';
+import { FormGroup, Button, Input, Label, Table } from 'reactstrap';
 import { EntityBase } from '../common';
 import EntityMenu from '../common/EntityMenu';
 import localeService from '../../services/localeService';
 
 const FieldsView = ({updateField, item, translations}) => {
   return <React.Fragment>
-    {_.keys(translations).map((i, index)=> (
-    <FormGroup key={`${i}${index}`}>
-      <Label htmlFor="name">{i}</Label>
-      <Input
-        id="language"
-        type="Text"
-        value={item.translations[i]}
-        onChange={(event) => updateField(`translations.${i}`, event.target.value)}
-      />
-    </FormGroup>))}
+    <Table bordered striped hover>
+      <thead>
+        <tr>
+          <th width={"30%"}>
+            Key
+          </th>
+          <th>
+            Value
+          </th>
+        </tr>
+      </thead>
+    {_.keys(translations).map((i, index) => (
+      <tr key={`${i}${index}`}>
+        <td>
+          <Label htmlFor="name">{i}</Label>
+        </td>
+        <td>
+          <Input
+            id="language"
+            type="Text"
+            value={item.translations[i]}
+            onChange={(event) => updateField(`translations.${i}`, event.target.value)}
+          />
+        </td>
+    </tr>
+    ))}
+    </Table>
   </React.Fragment>
 }
 
-const JSONView = ({updateField, item, translations}) => {
+const JSONView = ({updateField, item}) => {
   return <FormGroup>
   <Label>Translations</Label>
   <Input
@@ -40,7 +57,9 @@ class Locale extends EntityBase {
     this.entityService = localeService;
     this.state = {
       ...this.state,
-      view: 'field'
+      view: 'field',
+      newTranslationField: false,
+      translationField: {}
     }
   }
 
@@ -49,8 +68,29 @@ class Locale extends EntityBase {
     this.setState({view})
   }
 
+  toggleTranslationField = () => {
+    const {newTranslationField} = this.state;
+    this.setState({newTranslationField: !newTranslationField, translationField: {}})
+  }
+
+  saveTranslationField = () => {
+    const {translationField} = this.state;
+    const {key, value} = translationField;
+    this.updateField(`translations.${key}`, value)
+    this.setState({
+      translationField: {},
+      newTranslationField: false
+    });
+  }
+
+  updateTranslationField = (name, value) => {
+    const { translationField } = this.state;
+    const updated = _.set(translationField, name, value);
+    this.setState({ translationField: updated });
+  }
+
   render() {
-    const { item } = this.state;
+    const { item, translationField, newTranslationField} = this.state;
     const { translations } = item;
     return (
       <div className="animated fadeIn">
@@ -61,7 +101,7 @@ class Locale extends EntityBase {
         <Button onClick={this.toggleView}>{this.state.view}</Button>
 
         <FormGroup>
-          <Label htmlFor="language">language</Label>
+          <Label htmlFor="language">Language</Label>
           <Input
             id="language"
             type="Text"
@@ -70,6 +110,31 @@ class Locale extends EntityBase {
           />
         </FormGroup>
         <hr></hr>
+        <Button onClick={this.toggleTranslationField}>Add Translation</Button>
+        {newTranslationField ? 
+          <React.Fragment> 
+            <FormGroup>
+              <Label htmlFor="translationField">Key</Label>
+              <Input
+                id="translationField"
+                type="Text"
+                value={translationField.key}
+                onChange={(event) => this.updateTranslationField("key", event.target.value)}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="translationValue">Value</Label>
+              <Input
+                id="translationValue"
+                type="Text"
+                value={translationField.value}
+                onChange={(event) => this.updateTranslationField("value", event.target.value)}
+              />
+            </FormGroup>
+            <Button onClick={this.saveTranslationField}>Save</Button> 
+          </React.Fragment>
+        : null}
+        <hr></hr>
         {this.state.view === 'field'? <FieldsView 
           updateField={this.updateField.bind(this)}
           translations={translations}
@@ -77,7 +142,6 @@ class Locale extends EntityBase {
         </FieldsView> : 
         <JSONView 
           updateField={this.updateField.bind(this)}
-          translations={translations}
           item={item}>
         </JSONView>
         }
