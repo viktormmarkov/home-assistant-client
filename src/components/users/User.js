89a4c0f494d94a55125777d3d5fcb8188f29b079
Victor Markov <victor.markov@urbanise.com>
2020-12-16 15:59:57 +0200

    Update profile picture

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import userService from '../../services/userService';
+import { Button } from 'reactstrap';
 
 class User extends Component {
   constructor(context) {
@@ -8,7 +9,8 @@ class User extends Component {
     this.state = {
       userId: this.props.match.params.id,
       user: {},
-      loading: false
+      loading: false,
+      selectedFile: null
     };
   }
   componentDidMount() {
@@ -25,11 +27,31 @@ class User extends Component {
         this.setState({loading: false});
       })
   }
+  uploadImage = () => {
+    const formData = new FormData;
+    formData.append('image', this.state.selectedFile, this.state.selectedFile.name);
+    return userService.saveProfilePicture(formData).then(data => {
+      console.log(data);
+    })
+  }
+  fileSelectedChanged = () => {
+    this.setState({
+      selectedFile: event.target.files[0]
+    })
+  }
+
   render() {
     const {user} = this.state
     return (
       <div className="animated fadeIn">
           <h1>User {user.name}</h1>
+          <img src={user.profilePicture}>
+          </img>
+          <input type="file" accept="image/*" onChange={this.fileSelectedChanged}></input>
+         
+          <Button onClick={this.uploadImage} className="btn-sm" color="primary">
+            Upload
+          </Button>
       </div>
     );
   }
--- src/services/userService.js
+++ src/services/userService.js
@@ -4,6 +4,15 @@ class UserService extends ServiceBase {
     constructor() {
         super('users');
     }
+    getProfile = () => {
+        return this.api.get(`/${this.entity}/current/profile`).then(res => res.data);
+    }
+    saveProfile = (update) => {
+        return this.api.put(`/${this.entity}/current/profile`, update).then(res => res.data);
+    }
+    saveProfilePicture = (picture) => {
+        return this.api.post(`/${this.entity}/current/profile/picture`, picture).then(res => res.data);
+    }
 }
 
 export default new UserService();
